f5bac5af3f8d425929976b7ee8f197349a4183f3
xymetasearch <xymetasearch@gmail.com>
2022-02-11 20:06:25 +0900

    Add files via upload
    change f1 score to f2 score.

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -44,10 +44,10 @@ class Loggers():
         self.logger = logger  # for printing results to console
         self.include = include
         self.keys = ['train/box_loss', 'train/obj_loss', 'train/cls_loss',  # train loss
-                     'metrics/precision', 'metrics/recall', 'metrics/mAP_0.5', 'metrics/mAP_0.5:0.95',  # metrics
+                     'metrics/precision', 'metrics/recall', 'metrics/F2', 'metrics/mAP_0.5', 'metrics/mAP_0.5:0.95',  # metrics
                      'val/box_loss', 'val/obj_loss', 'val/cls_loss',  # val loss
                      'x/lr0', 'x/lr1', 'x/lr2']  # params
-        self.best_keys = ['best/epoch', 'best/precision', 'best/recall', 'best/mAP_0.5', 'best/mAP_0.5:0.95',]
+        self.best_keys = ['best/epoch', 'best/precision', 'best/recall', 'best/F2', 'best/mAP_0.5', 'best/mAP_0.5:0.95',]
         for k in LOGGERS:
             setattr(self, k, None)  # init empty logger dictionary
         self.csv = True  # always log to csv
@@ -127,7 +127,8 @@ class Loggers():
 
         if self.wandb:
             if best_fitness == fi:
-                best_results = [epoch] + vals[3:7]
+                #best_results = [epoch] + vals[3:7]
+                best_results = [epoch] + vals[3:8]
                 for i, name in enumerate(self.best_keys):
                     self.wandb.wandb_run.summary[name] = best_results[i]  # log best results in the summary
             self.wandb.log(x)
